14f7346c0e84cdb2f6207108f714e27354e962f3
raksha82 <rakshamurugesan82@gmail.com>
2026-09-22 22:54:16 +0530

    Files added

---
+++ To-Do List/script.js
@@ -0,0 +1,24 @@
+let task=document.getElementById("task-input");
+let error=document.getElementById("errormsg");
+let reset=document.getElementById("resetbutton");
+function addTask()
+{
+   let task_value=task.value;
+
+   if(task_value === "")
+   {
+    showerror("Enter the task");
+   }
+}
+
+function showerror(msg)
+{
+   error.style.color="red";
+   error.textContent=msg;
+}
+
+
+reset.addEventListener("click",()=>
+{
+    task.value="";
+})
