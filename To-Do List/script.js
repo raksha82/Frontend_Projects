eaeabc9a3997c3df96cc60c085cce3f29b016d26
raksha82 <rakshamurugesan82@gmail.com>
2026-09-23 11:07:53 +0530

    To-Do

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -2,21 +2,33 @@ let task=document.getElementById("task-input");
 let error=document.getElementById("errormsg");
 let reset=document.getElementById("resetbutton");
 let success=document.getElementById("successmsg");
-let count=0;
+let list=document.getElementById("task-container");
+let complete=document.getElementById("complete");
+let uncomplete=document.getElementById("notcompleted");
+var uncompleted=0;
+var completed=0;
 function addTask()
 {
    let task_value=task.value;
 
-   if(task_value === "")
+   if(task_value.trim() === "")
    {
     showerror("Enter the task");
    }
 
-   count++;
+   else
+   {
+   uncompleted++;
+   list.innerHTML += `
+    <div>
+        <input type="checkbox" onchange="taskStatus(this)">
+        <span>${task_value}</span>
+    </div>
+`;
+task.value = "";
    success.style.color="green";
-   success.textContent=`${count} Task Added Successfully`;
-   
-
+   success.textContent=`${uncompleted} Task Added Successfully`;
+   }
 
 }
 
@@ -33,3 +45,24 @@ reset.addEventListener("click",()=>
     error.textContent="";
     success.textContent="";
 })
+
+function taskStatus(checkbox)
+{
+    let taskText = checkbox.nextElementSibling;
+
+    if (checkbox.checked)
+    {
+        completed++;
+        uncompleted--;
+        taskText.style.textDecoration = "line-through";
+    }
+    else
+    {
+        completed--;
+        uncompleted++;
+        taskText.style.textDecoration = "none";
+    }
+}
+
+complete.textContent=`Completed ${completed}`
+uncomplete.textContent=`UnCompleted ${uncompleted}`
