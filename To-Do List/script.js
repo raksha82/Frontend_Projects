b4445f1902d19754b8c5417974014d0da3bd6657
raksha82 <rakshamurugesan82@gmail.com>
2026-09-23 11:37:00 +0530

    To-Do

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -25,6 +25,9 @@ function addTask()
     <div class="task-item">
         <input type="checkbox" onchange="taskStatus(this)">
         <span>${task_value}</span>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" onclick="removetask(this)" class="bi bi-x" viewBox="0 0 16 16">
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+        </svg>
     </div>
 `;
    updateCount();
@@ -45,8 +48,27 @@ reset.addEventListener("click",()=>
     task.value="";
     error.textContent="";
     success.textContent="";
+    list.innerHTML="";
+    completed=0;
+    uncompleted=0;
+    updateCount();
 })
 
+
+function removetask(x)
+{
+   x.parentElement.remove();
+   if(uncompleted>0)
+        {
+        uncompleted--;
+        }
+
+        else
+        {
+         uncompleted=0;
+        }
+   updateCount();
+}
 function taskStatus(checkbox)
 {
     let taskText = checkbox.nextElementSibling;
@@ -54,7 +76,15 @@ function taskStatus(checkbox)
     if (checkbox.checked)
     {
         completed++;
+        if(uncompleted>0)
+        {
         uncompleted--;
+        }
+
+        else
+        {
+         uncompleted=0;
+        }
         taskText.style.textDecoration = "line-through";
         
     }
