e47af46dfa8acb6bae456d3e875fc559450df80b
raksha82 <rakshamurugesan82@gmail.com>
2026-09-25 14:01:49 +0530

    React

--- To-Do List React/To-Do/src/Form.jsx
+++ To-Do List React/To-Do/src/Form.jsx
@@ -3,7 +3,9 @@ import './Form.css'
 function Form()
 {
     return <div className="form">
+    
     <form className="form-Container">
+        <label>Input Box</label>
         <input placeholder="Enter the Title" type="text" min={1}></input>
         <textarea type="text" placeholder="Description"></textarea>
         <button>Add Task</button>
